2778951db955bc238ac6b837985ef928af33c336
MatthiasWittl <Matthias_Wittl@outlook.de>
2025-11-24 18:30:00 +0100

    js db added

---
+++ data/db.js
@@ -0,0 +1,110 @@
+let Menu = {
+    appetizer: [
+    {   "id": 1,
+        "name": "Bruschetta",
+        "description": "geröstetes Brot mit Tomaten, Basilikum, Knoblauch und Olivenöl",
+        "price": 5.50
+        },
+    {
+        "id": 2,
+        "name": "Caprese",
+        "description": "Tomaten-Mozzarella-Salat mit Basilikum",
+        "price": 6.00
+        },
+    {
+        "id": 3,
+        "name": "Antipasti-Platte",
+        "description": "Auswahl an italienischen Vorspeisen",
+        "price": 8.50
+    },
+    {
+        "id": 4,
+        "name": "Carpaccio",
+        "description": "dünn geschnittenes Rindfleisch mit Parmesan und Rucola",
+        "price": 4.50
+    },
+    {
+        "id": 5,
+        "name": "Bavarese alla Spuma",
+        "description": "frisches Weißbier mit einer leichten Schaumkrone und zarten Weißwürstchen",
+        "price": 7.00
+    }
+    ],
+    mainCourse: [
+        {
+            "id": 30,
+            "name": "Margherita",
+            "description": "Tomatensauce, Mozzarella, frisches Basilikum",
+            "price": 8.00
+        },
+        {
+            "id": 31,
+            "name": "Pepperoni",
+            "description": "Tomatensauce, Mozzarella, scharfe Peperoni",
+            "price": 9.50
+        },
+        {
+            "id": 32,
+            "name": "Quattro Stagioni", 
+            "description": "Tomatensauce, Mozzarella, Schinken, Pilze, Artischocken, Oliven",
+            "price": 10.00                            
+        }, 
+        {
+            "id": 33,
+            "name": "Vegetariana",
+            "description": "Tomatensauce, Mozzarella, Paprika, Zucchini, Auberginen, Pilze",
+            "price": 9.00
+        },
+        {
+            "id": 34,
+            "name": "Bavaria",
+            "description": "Tomatensauce, Schafskäse, hauchzarte Leberkäsescheiben, Bärlauchcreme",
+            "price": 10.50
+        },
+        {
+            "id": 35,
+            "name": "Hawaiian",
+            "description": "Tomatensauce, Mozzarella, Schinken, Ananas",
+            "price": 9.50
+        }
+    ],
+    dessert: [
+        {
+            "id": 50,
+            "name": "Tiramisu",
+            "description": "klassisches italienisches Dessert mit Mascarpone, Espresso und Kakao",
+            "price": 4.50
+        },
+        {
+            "id": 51,
+            "name": "Panna Cotta",
+            "description": "cremiger Vanillepudding mit Beerensauce",
+            "price": 4.00
+        },
+        {
+            "id": 52,
+            "name": "Gelato Misto",
+            "description": "verschiedene Sorten italienisches Eis",
+            "price": 3.50
+        },
+        {
+            "id": 53,
+            "name": "Cannoli",
+            "description": "knusprige Teigröllchen gefüllt mit süßer Ricotta-Creme",
+            "price": 4.20
+        },
+        {
+            "id": 54,
+            "name": "Affogato",
+            "description": "Vanilleeis mit heißem Espresso übergossen",
+            "price": 4.80
+        },
+        {
+            "id": 55,
+            "name": "Birra Bianca Cremosa",
+            "description": "gekühltes Weißbier mit luftiger Schaumkrone",
+            "price": 3.50
+        }
+
+    ]
+}
