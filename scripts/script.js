83e598b78d33e2ef97a4181efcb3ef36571b273b
MatthiasWittl <Matthias_Wittl@outlook.de>
2025-11-26 20:57:08 +0100

    img change by changing menu added

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@
 
 function renderDishes(menu) {
     document.getElementById("all_dishes").innerHTML = "";
+    document.getElementById("actual_menu_image_change").src = "assets/Images/" + [menu] + ".jpg";
     for (let index = 0; index < Menu[menu].length; index++) {
         document.getElementById("all_dishes").innerHTML += addDishes(index, menu);
         
