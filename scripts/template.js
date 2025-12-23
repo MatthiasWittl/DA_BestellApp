daa5f9926220b186380d1f90c2ae382ddc589a95
MatthiasWittl <Matthias_Wittl@outlook.de>
2025-12-23 09:18:10 +0100

    template js refactored styling

--- scripts/template.js
+++ scripts/template.js
@@ -1,45 +1,46 @@
 function renderDishes(index, menu) {
   return `
-        <div class="dish_menu" >
-                    <div>
-                        <h2>${Menu[menu][index].name}</h2>
-                        <p>${Menu[menu][index].description}</p>
-                        <p>${Menu[menu][index].price.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
-                    </div>
-                    <button class="add_to_cart" onclick="addToCart(${[index,]}, '${menu}')" aria-label="${Menu[menu][index].name} hinzufügen" type="button">
-                    <span aria-hidden="true">&#0043</span></button>
-                </div>`;
+            <div class="dish_menu" >
+                <div>
+                    <h2>${Menu[menu][index].name}</h2>
+                    <p>${Menu[menu][index].description}</p>
+                    <p>${Menu[menu][index].price.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
+                </div>
+                <button class="add_to_cart" onclick="addToCart(${[index,]}, '${menu}')" aria-label="${Menu[menu][index].name} hinzufügen" type="button">
+                <span aria-hidden="true">&#0043</span></button>
+            </div>
+            `;
 }
 
 function renderCart(index) {
   return `
-        <div class="cart_dishes">
-        <div>
-        <h3>${cart.item[index].name}</h3>
-        <button class="cart_add_remove_pieces" onclick="countPieces('down', ${[index,]})" >&#8722</button>
-        <span class="cart_pieces" aria-label="${cart.item[index].piece} Stück ${cart.item[index].name}" ><span aria-hidden="true">${cart.item[index].piece}</span></span>
-        <button class="cart_add_remove_pieces" onclick="countPieces('up', ${[index,]})" >&#43</button>
-        </div>
-        <span class="cart_dish_price" >${cart.item[index].price.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</span>
-    </div>
-        `;
+            <div class="cart_dishes">
+                <div>
+                    <h3>${cart.item[index].name}</h3>
+                    <button class="cart_add_remove_pieces" onclick="countPieces('down', ${[index,]})" >&#8722</button>
+                    <span class="cart_pieces" aria-label="${cart.item[index].piece} Stück " ><span aria-hidden="true">${cart.item[index].piece}</span></span>
+                    <button class="cart_add_remove_pieces" onclick="countPieces('up', ${[index,]})" >&#43</button>
+                </div>
+                <span class="cart_dish_price" >${cart.item[index].price.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</span>
+            </div>
+            `;
 }
 
 function renderCartCost() {
     return `
-        <p class="free_delivery_container" id="free_delivery"> Noch ${differenceToFreeDelivery.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})} bis Lieferkosten frei </p>
-       <div>
-                    <p>Zwischensumme</p>
-                    <p>${cartItemCost.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
-                </div>
-                <div class="cart_border_bottom">
-                    <p>Lieferkosten</p>
-                    <p>${deliveryFee.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
-                </div>
-                <div  >
-                    <p>Gesamt</p>
-                    <p>${cartTotalCost.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
-        </div>
-        <button class="cart_order_button" onclick="openDialog()" type="button">Bezahlen</button>
-        `
+            <p class="free_delivery_container" id="free_delivery"> Noch ${differenceToFreeDelivery.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})} bis Lieferkosten frei </p>
+            <div>
+                <p>Zwischensumme</p>
+                <p>${cartItemCost.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
+            </div>
+            <div class="cart_border_bottom">
+                <p>Lieferkosten</p>
+                <p>${deliveryFee.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
+            </div>
+            <div>
+                <p>Gesamt</p>
+                <p>${cartTotalCost.toLocaleString("de-DE", { style: "currency", currency: "EUR", maximumFractionDigits: 1})}</p>
+            </div>
+            <button class="cart_order_button" onclick="openDialog()" type="button">Bezahlen</button>
+            `;
 }
